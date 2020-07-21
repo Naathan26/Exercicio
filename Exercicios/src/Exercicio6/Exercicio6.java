575eacb5b27bdeaf5a593857feb3bf5d5d23c0e7
diogenesistemas <ads.diogenes@gmail.com>
2020-07-21 09:28:03 -0300

    Atualizando

--- Exercicios/src/Exercicio6/Exercicio6.java
+++ Exercicios/src/Exercicio6/Exercicio6.java
@@ -4,10 +4,12 @@ public class Exercicio6 {
 
 	public static void main(String[] args) {
 	
-		String Encapsulamento = a técnica que faz com que detalhes internos do funcionamento dos métodos de uma classe permaneçam ocultos para os objetos;
-		String Polimorfismo = um princípio a partir do qual as classes derivadas de uma única classe base são capazes de invocar os métodos;
-		System.out.println("Encapsulamento eh:" + Encapsulamento n/"Polimorfismo eh:"+Polimorfismo )
-
+		String e = " A técnica que faz com que detalhes internos do funcionamento dos métodos de uma classe permaneçam ocultos para os objetos.";
+		String p = " O principal conceito é a propriedade de duas ou mais classes derivadas de uma mesma superclasse responderem a mesma mensagem.";
+		
+		System.out.println("Encapsulamento eh :" + e);
+		System.out.println("Polimorfismo eh:" + p);
+		
+	
 	}
-
-}
+	}
